d88fd546213dcd7dabfbc8990efc09ae45da5b81
muhammad.tama <direktoratctf@Rustams-MacBook-Pro.local>
2021-01-23 12:58:07 +0700

    change controller

--- src/main/java/com/example/juniorprogrammer/cicdtomcat/controllers/CheckController.java
+++ src/main/java/com/example/juniorprogrammer/cicdtomcat/controllers/CheckController.java
@@ -1,14 +1,15 @@
 package com.example.juniorprogrammer.cicdtomcat.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CheckController {
 
     @GetMapping(value = "test")
-    public String check(){
-        return "masuk pak eko";
+    public String check(@RequestParam String name){
+        return "masuk pak "+name;
     }
 
 }
